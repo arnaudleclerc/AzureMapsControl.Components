ea7051366da151d9b53ab00b2b83e078a8897ab0
Arnaud Leclerc <A.Leclerc@tekaris.com>
2020-11-09 11:32:21 +0100

    Constants should not be in extensions

--- src/AzureMapsControl.Components/Constants/Constants.cs
+++ src/AzureMapsControl.Components/Constants/Constants.cs
@@ -9,5 +9,19 @@
         /// JsInterop namespace
         /// </summary>
         internal const string Namespace = "azureMapsControl";
+
+        internal const string MethodAddMap = "addMap";
+        internal const string MethodAddControl = "addControls";
+        internal const string MethodAddHtmlMarkers = "addHtmlMarkers";
+        internal const string MethodSetOptions = "setOptions";
+        internal const string MethodRemoveHtmlMarkers = "removeHtmlMarkers";
+        internal const string MethodUpdateHtmlMarkers = "updateHtmlMarkers";
+        internal const string MethodAddDrawingToolbar = "addDrawingToolbar";
+        internal const string MethodUpdateDrawingToolbar = "updateDrawingToolbar";
+        internal const string MethodAddLayer = "addLayer";
+        internal const string MethodRemoveLayers = "removeLayers";
+        internal const string MethodAddDataSource = "addDataSource";
+        internal const string MethodDataSourceImportDataFromUrl = "dataSource_importDataFromUrl";
+        internal const string MethodDataSourceAdd = "dataSource_add";
     }
 }
