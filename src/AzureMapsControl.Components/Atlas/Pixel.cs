f32eb22dfd05ea7f832be3386c68840e5e89c909
Arnaud Leclerc <A.Leclerc@tekaris.com>
2021-01-03 12:24:58 +0100

    Updating options on markers when an event is trigerred

--- src/AzureMapsControl.Components/Atlas/Pixel.cs
+++ src/AzureMapsControl.Components/Atlas/Pixel.cs
@@ -1,11 +1,15 @@
 ﻿namespace AzureMapsControl.Components.Atlas
 {
+    using System;
     using System.Diagnostics.CodeAnalysis;
+    using System.Text.Json;
+    using System.Text.Json.Serialization;
 
     /**
      * Represent a pixel coordinate or offset.
      */
     [ExcludeFromCodeCoverage]
+    [JsonConverter(typeof(PixelJsonConverter))]
     public sealed class Pixel
     {
         /// <summary>
@@ -28,6 +32,32 @@
         {
             X = x;
             Y = y;
+        }
+    }
+
+    internal sealed class PixelJsonConverter : JsonConverter<Pixel>
+    {
+        public override Pixel Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var pixel = new Pixel();
+            if (reader.TokenType == JsonTokenType.StartArray)
+            {
+                reader.Read();
+                pixel.X = reader.GetDouble();
+                reader.Read();
+                pixel.Y = reader.GetDouble();
+                reader.Read();
+            }
+
+            return pixel;
+        }
+
+        public override void Write(Utf8JsonWriter writer, Pixel value, JsonSerializerOptions options)
+        {
+            writer.WriteStartArray();
+            writer.WriteNumberValue(value.X);
+            writer.WriteNumberValue(value.Y);
+            writer.WriteEndArray();
         }
     }
 }
--- src/AzureMapsControl.Components/Markers/HtmlMarkerJsEventArgs.cs
+++ src/AzureMapsControl.Components/Markers/HtmlMarkerJsEventArgs.cs
@@ -1,11 +1,12 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace AzureMapsControl.Components.Markers
+﻿namespace AzureMapsControl.Components.Markers
 {
+    using System.Diagnostics.CodeAnalysis;
+
     [ExcludeFromCodeCoverage]
     public sealed class HtmlMarkerJsEventArgs
     {
         public string MarkerId { get; set; }
         public string Type { get; set; }
+        public HtmlMarkerOptions Options { get; set; }
     }
 }
