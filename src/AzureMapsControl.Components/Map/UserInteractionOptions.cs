cacb280ae28a3bf3ef2e452a12a2771b6ca404d8
Arnaud Leclerc <aleclerc1@gmail.com>
2021-01-24 10:18:21 +0100

    Using default values of azure-maps for user interactions (#28)

--- src/AzureMapsControl.Components/Map/UserInteractionOptions.cs
+++ src/AzureMapsControl.Components/Map/UserInteractionOptions.cs
@@ -6,48 +6,48 @@
     public sealed class UserInteractionOptions
     {
         /// <summary>
-        /// Whether the Shift + left click and drag will draw a zoom box. default true
+        /// Whether the Shift + left click and drag will draw a zoom box.
         /// </summary>
-        public bool BoxZoomInteraction { get; set; } = true;
+        public bool? BoxZoomInteraction { get; set; }
 
         /// <summary>
-        /// Whether double left click will zoom the map inwards. Default true
+        /// Whether double left click will zoom the map inwards.
         /// </summary>
-        public bool DblclickZoomInteraction { get; set; } = true;
+        public bool? DblclickZoomInteraction { get; set; }
 
         /// <summary>
-        /// Whether left click and drag will pan the map. default true
+        /// Whether left click and drag will pan the map.
         /// </summary>
-        public bool DragPanInteraction { get; set; } = true;
+        public bool? DragPanInteraction { get; set; }
 
         /// <summary>
-        /// Whether right click and drag will rotate and pitch the map. default true
+        /// Whether right click and drag will rotate and pitch the map.
         /// </summary>
-        public bool DragRotateInteraction { get; set; } = true;
+        public bool? DragRotateInteraction { get; set; }
 
         /// <summary>
-        /// Whether the map is interactive or static. If false, all user interaction is disabled. If true, only selected user interactions will enabled. default true
+        /// Whether the map is interactive or static. If false, all user interaction is disabled. If true, only selected user interactions will enabled.
         /// </summary>
-        public bool Interactive { get; set; } = true;
+        public bool? Interactive { get; set; }
 
         /// <summary>
-        /// Whether the keyboard interactions are enabled. Default true
+        /// Whether the keyboard interactions are enabled.
         /// </summary>
-        public bool KeyboardInteraction { get; set; } = true;
+        public bool? KeyboardInteraction { get; set; }
 
         /// <summary>
-        /// Whether the map should zoom on scroll input. default true
+        /// Whether the map should zoom on scroll input.
         /// </summary>
-        public bool ScrollZoomInteraction { get; set; } = true;
+        public bool? ScrollZoomInteraction { get; set; }
 
         /// <summary>
-        /// Whether touch interactions are enabled for touch devices. default true
+        /// Whether touch interactions are enabled for touch devices.
         /// </summary>
-        public bool TouchInteraction { get; set; } = true;
+        public bool? TouchInteraction { get; set; }
 
         /// <summary>
-        /// Sets the zoom rate of the mouse wheel default 1/450
+        /// Sets the zoom rate of the mouse wheel
         /// </summary>
-        public double WheelZoomRate { get; set; }
+        public double? WheelZoomRate { get; set; }
     }
 }
