c9a441ede47363d502e025eb9f111842fb913eb1
Arnaud Leclerc <A.Leclerc@tekaris.com>
2021-04-13 14:53:06 +0200

    Fix linting

--- src/AzureMapsControl.Components/typings/GeolocationControl/index.d.ts
+++ src/AzureMapsControl.Components/typings/GeolocationControl/index.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/member-ordering */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable quotes */
 /* eslint-disable @typescript-eslint/prefer-namespace-keyword */
