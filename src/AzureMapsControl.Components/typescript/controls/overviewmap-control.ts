34956f049a08264af16840de9fabeb5fc56efe48
Roope Herpiö <roope.herpio@mtech.fi>
2025-11-03 11:06:58 +0200

    Fix issue with overviewmap sometimes not changing style with the map.

--- src/AzureMapsControl.Components/typescript/controls/overviewmap-control.ts
+++ src/AzureMapsControl.Components/typescript/controls/overviewmap-control.ts
@@ -3,6 +3,9 @@ import { Core } from '../core/core';
 
 export class OverviewMapControl {
     public static setOptions(mapId: string, controlId: string, options: overviewmap.OverviewMapOptions): void {
+        if(options.style == null || options.style["indexOf"] == null){
+            options.style = "auto";
+        }
         (Core.getMap(mapId).controls.getControls().find(ctrl => (ctrl as any).amc && (ctrl as any).amc.id === controlId) as overviewmap.control.OverviewMap).setOptions(options);
     }
 }
